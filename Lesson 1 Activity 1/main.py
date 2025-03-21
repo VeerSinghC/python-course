064b2b1b930bd948634da43c4a608825e07f64d7
Veer Singh <v1014singh@gmail.com>
2025-03-21 20:19:59 +0530

    ini

---
+++ Lesson 1 Activity 1/main.py
@@ -0,0 +1,4 @@
+print("This is the new module in which we are gonna study\nAbout python")
+print("Our teacher is Miss Ekta Jha who is a very Hunble, Polite and co-operating teacher.")
+print("with my companion Master Abhilashan I wish this module goes well")
+print("signing off Veer Singh")
