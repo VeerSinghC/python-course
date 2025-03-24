e8df1d9c4befed75cd830ef0698b3e3a4fa16aa8
Veer Singh <v1014singh@gmail.com>
2025-03-24 20:52:29 +0530

    in

---
+++ operators and data types/python opertors.py
@@ -0,0 +1,20 @@
+num1 = 99
+num2 = 199
+
+print("Number 1", num1)
+print("Number 2", num2)
+print("Addition: ", num1+num2)
+print("Difference ", num2-num1)
+print("Product: ", num1*num2)
+print("DIvision: ", num1/num2)
+print("Floor Division: ", num1//num2)
+print("Modulus Operation: ", num1%num2)
+print("Square", num2**num2)
+print("Square Root", num1**0.5)
+print("Equal ?", num1==num2)
+print("Number 1 greater", num1>num2)
+print("Number 2 greater", num1<num2)
+print("Not Equal ?", num1!=num2)
+
+result = num1/2+num2**2+10
+print("result of given equation is:", result)
