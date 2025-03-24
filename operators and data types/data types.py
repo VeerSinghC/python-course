a92f7340b2019dec328814fc100e0196264ecb79
Veer Singh <v1014singh@gmail.com>
2025-03-24 20:39:11 +0530

    im

---
+++ operators and data types/data types.py
@@ -0,0 +1,13 @@
+name = "Pikachu"
+age = 15
+is_student = True
+weight = 0.99
+
+print("Name:, name")
+print("Data type of name is", type(name))
+print("age:, age")
+print("Data type of age is", type(age))
+print("is_student:, is_student")
+print("Date type of is_sutdent", type(is_student))
+print("weight:, weight")
+print("data type of weight is", type(weight))
