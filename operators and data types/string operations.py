de636916f1ac4358c6f8f184389cc5358f434b61
Veer Singh <v1014singh@gmail.com>
2025-03-30 12:03:46 +0530

    in

---
+++ operators and data types/string operations.py
@@ -0,0 +1,15 @@
+first_name = "Codingal"
+last_name = "Singh"
+full_name = first_name+last_name
+example = "Ha"*5
+
+print("First name: ", first_name)
+print("Last name", last_name)
+print("Full name", full_name)
+print("String multiplied 5 time gives this result: ", example)
+
+word = 'coding'
+print("Length of string : ", len(word))
+print("First Letter of the String : ", word[0])
+print("Last letter of the string : ", word[5])
+print("String Sliced : ", word[0:3])
