8f8d850def3f76c609273585134e70474bdc6b44
Veer Singh <v1014singh@gmail.com>
2025-05-09 20:10:14 +0530

    codi

---
+++ operations of file -1.py/read operations -1.py
@@ -0,0 +1,12 @@
+file = open('read.txt','r')
+print(file.read())
+file.close()
+
+file = open('read.txt','r')
+print("\n Read in parts \n")
+print(file.read(8))
+file.close()
+
+file = open('read.txt','a')
+file.write("Hi! I am MUNIR and I am 1.5 years old")
+file.close()    
